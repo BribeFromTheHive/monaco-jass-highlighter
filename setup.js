cd175bfccedd53e7a59078cc5ad8d69ca6e1a711
Luke Kelly <lukeckelly@gmail.com>
2023-08-09 12:47:29 +0200

    Updated setupJass function to accept wasmPath parameter.

--- setup.js
+++ setup.js
@@ -2,8 +2,19 @@ import { Registry } from 'monaco-textmate';
 import { wireTmGrammars } from 'monaco-editor-textmate';
 import * as monaco from 'monaco-editor';
 import jassGrammar from './syntaxes/jass.tmlanguage.json'
+import { loadWASM } from "onigasm";
 
-export async function setupJass() {
+/**
+ * Initializes and sets up JASS highlighting for Monaco.
+ *
+ * @function
+ * @async
+ * @param {string} wasmPath - The path to the onigasm WebAssembly binary.
+ * @returns {Promise<void>} A promise that resolves when setup is complete.
+ */
+export async function setupJass(wasmPath) {
+    await loadWASM(wasmPath)
+
     const registry = new Registry({
         getGrammarDefinition: async (scopeName) => {
             if (scopeName === 'source.jass') {
